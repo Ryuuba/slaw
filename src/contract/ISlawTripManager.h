4098508c0b56f316db8d6b6686bfca5c5f2e1f14
Adan G. Medrano-Chavez <ryuuba@gmail.com>
2019-08-16 18:12:04 -0500

    Update README.md

---
+++ src/contract/ISlawTripManager.h
@@ -0,0 +1,79 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#if !defined(I_SLAW_TRIP_MANAGER)
+#define I_SLAW_TRIP_MANAGER
+
+#include "../common/SlawDefs.h"
+#include "../map/SelfsimilarWaypointMap.h"
+#include "../extra/SpeedModel.h"
+#include "../extra/PauseTimeModel.h"
+#include "../latp/LATP.h"
+
+class ISlawTripManager: public omnetpp::cSimpleModule{
+protected:
+  /** @brief the number of walkers in a simulation */
+  unsigned walkerNum;
+  /** @brief Temporal data structure to store the C_k set of each walker */
+  std::vector<AreaSet> CkSet;
+  /** @brief Planning degree */
+  double planningDegree;
+  /** @brief The least-action trip-planning algorithm */
+  LATPAlgorithm latp;
+  /** @brief The name of the individual walker model */
+  const char* walker_model;
+protected:
+  /** @brief Loads a file containing the C_k set of each walker */
+  void loadCKFile(char const*);
+  /** @brief Initializes the self-similar waypoint map. */
+  virtual void setMap();
+  /** @brief Initializes the pausetime model */
+  virtual void setPauseTimeModel();
+  /** @brief Initializes the speed model */
+  virtual void setSpeedModel();
+  /** @brief Computes a new destination waypoint list according to the values of
+   *  the member variable model. This member function is overridden */
+  virtual WaypointList computeDestinationList(
+    const AreaSet&, inet::Coord&
+  ) = 0;
+  /** @brief Assigns the confined areas of all walkers. It must be overridden */
+  virtual void assignConfinedAreas() = 0;
+public:
+  /** @brief Object implementing the waypoint map */
+  SelfsimilarWaypointMap* const map = new SelfsimilarWaypointMap;
+  /** @brief Object implementing the speed model */ 
+  SpeedModel* const speedModel = new SpeedModel;
+  /** @brief Object implementing the pause-time model */
+  PauseTimeModel* const pauseTimeModel = new PauseTimeModel;
+public:
+/** @brief Default destructor */
+virtual ~ISlawTripManager();
+  /** @brief Sets the set of confined areas C_k, the initial set of unvisited 
+   *  waypoints and the initial waypoint from which a walker starts its trip. 
+   *  This member function must be overridden */
+  virtual void setWalkerState(
+    unsigned, AreaSet&, WaypointList&, inet::Coord&
+  ) = 0;
+  /** @brief Computes the next destination */
+  virtual inet::Coord getNextDestination(
+    WaypointList&, const AreaSet&, inet::Coord&, unsigned
+  ) = 0;
+  /** @brief Initializes the Trip Manager module. It must be overridden*/
+  virtual void initialize() = 0;
+  /** @brief Returns the name of the individual walker model*/
+  const char* getIndividualWalkerModelName() {return walker_model;}
+};
+
+#endif
---
+++ src/walkermodel/SlawTransNewt.cc
@@ -0,0 +1,119 @@
+#include "SlawTransNetw.h"
+
+Define_Module(SlawTransNetw);
+
+void SlawTransNetw::initialize() {
+  walker_model = par("walkerModel").stringValue();
+  walkerNum = par("numOfWalker");
+  planningDegree = par("planningDegree").doubleValue();
+  latp.setLATP(planningDegree, getRNG(0));
+  setMap();
+  setPauseTimeModel();
+  setSpeedModel();
+  std::string filename(par("clusterList").stringValue());
+  if (filename.compare("") != 0)
+    loadCKFile(filename.c_str());
+  else
+    assignConfinedAreas();
+  computeHome();
+}
+
+void SlawTransNetw::setWalkerState(
+  unsigned walkerId, AreaSet& C_k, WaypointList& L, inet::Coord& initialWaypoint
+) {
+  C_k = std::move(CkSet[walkerId]);
+  initialWaypoint = homeList[walkerId];
+  L = computeDestinationList(C_k, initialWaypoint);
+}
+
+WaypointList SlawTransNetw::computeDestinationList(
+  const AreaSet& C_k, inet::Coord& home
+) {
+  WaypointList uwl; //stands for unvisited waypoint list (uwl)
+  computeTripRandomness(uwl, C_k);
+  computeRutine(uwl, C_k, home);
+  return uwl;
+}
+
+void SlawTransNetw::computeHome() {
+  unsigned areaIndex;
+  unsigned waypointIndex;
+  for (unsigned i = 0; i < walkerNum; i++) {
+    areaIndex = ceil(uniform(0,1) * CkSet[i].size()) - 1;
+    waypointIndex = ceil(uniform(0,1) * map->getAreaSize(CkSet[i][areaIndex])) - 1;
+    homeList.push_back(map->getWaypoint(CkSet[i][areaIndex], waypointIndex));
+  }
+}
+
+void SlawTransNetw::computeTripRandomness(
+  WaypointList& uwl, const AreaSet& C_k
+) {
+  AreaSet::const_iterator areaIt;
+  unsigned rndAreaId;
+  do {
+    rndAreaId = intuniform(0, map->getNumberOfAreas()-1);
+    areaIt = std::find(C_k.begin(), C_k.end(), rndAreaId);
+  } while ( areaIt != C_k.end() );
+  map->randomizeArea(getRNG(0), rndAreaId);
+  auto area = map->getConfinedArea(rndAreaId);
+  double portion = uniform(0.05,0.1);
+  unsigned numOfRndWp = ceil(portion * area->size());
+  uwl.insert(uwl.begin(), area->begin(), area->begin() + numOfRndWp);
+}
+
+void SlawTransNetw::computeRutine(
+  WaypointList& uwl, const AreaSet& C_k, inet::Coord& home
+) {
+  AreaSet selectedAreas;
+  Area temp;
+  unsigned numberOfRutineAreas = unsigned(C_k.size());
+  unsigned discartedAreaIndex = intuniform(0, numberOfRutineAreas - 1);
+  unsigned tripSize = intuniform(120, 150) - 1;//minus the home waypoint
+  for (unsigned areaIndex = 0; areaIndex < numberOfRutineAreas; areaIndex++)
+    if (discartedAreaIndex != areaIndex)
+      selectedAreas.push_back(C_k.at(areaIndex));
+  for (auto& area : selectedAreas)
+    for (auto& waypoint : *(map->getConfinedArea(area)))
+      if (waypoint != home)
+        temp.push_back(waypoint);
+  unsigned rutineTripSize = tripSize - uwl.size();//uwl.size() equals rand wp
+  if (temp.size() < rutineTripSize)
+    for (auto& waypoint : temp)
+      uwl.push_back(waypoint);
+  else {
+    randomizeVector(getRNG(0), temp);
+    for (int i = 0; i < rutineTripSize; i++)
+      uwl.push_back(temp[i]);
+  }
+}
+
+void SlawTransNetw::assignConfinedAreas() {
+  unsigned index, clusterID;
+  const std::vector<unsigned>* weights = map->getAreaWeights();
+  for (unsigned i = 0; i < walkerNum; i++) {
+    unsigned confinedAreas = intuniform(3,5);
+    AreaSet C_k;
+    while (C_k.size() < confinedAreas) {
+      index = ceil(uniform(0,1) * weights->size()) - 1;
+      clusterID = (*weights)[index];
+      auto it = std::find(C_k.begin(), C_k.end(), clusterID);
+      if (it == C_k.end())
+        C_k.push_back(clusterID);
+    }
+    CkSet.push_back(std::move(C_k));
+  }
+}
+
+inet::Coord SlawTransNetw::getNextDestination(
+    WaypointList& uwl, const AreaSet& C_k, inet::Coord& lastWaypoint, 
+    unsigned walkerID
+) {
+  inet::Coord nextWaypoint;
+  if (uwl.empty()) {
+    nextWaypoint = homeList[walkerID];
+    uwl = std::move(computeDestinationList(C_k, homeList[walkerID]));
+  }
+  else
+    nextWaypoint = latp(lastWaypoint, uwl);
+  return nextWaypoint; 
+}
